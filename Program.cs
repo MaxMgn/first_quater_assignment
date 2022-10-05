72c943d205291c54bd2fd981354f5446e6b43e96
MaxMgn <m17111987@gmail.ru>
2022-10-05 14:54:56 +0500

    addeв_program

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿string [] formArrayWithElementsConstistingNoMoreThanThreeSymbols (string [] in_array){
+    string [] out_array = new string [in_array.Length];
+    int j = 0;
+    for (int i = 0; i < in_array.Length; i++)
+    {
+        if (in_array[i].Length < 4)
+        {
+            out_array [j] = in_array[i];
+            j++;
+        }
+    }
+    return out_array;
+}
+
+void printArray(string[] array)
+{   
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+             Console.Write("| "+array[i]+" |");
+    
+    }
+    Console.WriteLine();
+}
+
+string [] in_array = {"hello", "2", "world", ":-)"};
+
+string [] out_array = formArrayWithElementsConstistingNoMoreThanThreeSymbols (in_array);
+printArray(in_array);
+printArray(out_array);
+
